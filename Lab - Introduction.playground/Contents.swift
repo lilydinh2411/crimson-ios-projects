d1c066aff8f7789069aa047836d0ee4b6a13f0a2
lilydinh2411 <102395990+lilydinh2411@users.noreply.github.com>
2022-03-26 08:46:03 +0700

    Update Contents.swift

--- Lab - Introduction.playground/Contents.swift
+++ Lab - Introduction.playground/Contents.swift
@@ -8,15 +8,29 @@ print("I have learned the following:")
 print("What features make Swift a modern and safe language")
 print("How to use the Swift REPL in Terminal")
 print("How to use playgrounds to make writing Swift fun and simple")
- 
+
+print("In this lesson, I have learned about variables, constant, and static as well as arrays, sets, and keys")
+print("Variables' value can be changed while constant cannot")
+print("Arrays can have duplicated values while sets cannot")
+print("If two lists don't have anything in common or similiarties, then they are disjointed from each other")
+
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("One of my favorite songs is "thank u, next" by Ariana Grande.
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
-
+print("One taught me love")
+print("One taught me patience")      
+print("And one taught me pain")
+print("Now, I'm so amazing")
+print("Say I've loved and I've lost")
+print("But that's not what I see")
+print("So look what I got")
+print("Look at what you taught me")
+print("And for that, I say")
+print("Thank you, next (next)")      
+      
 //:page 14 of 16  |  [Next: Exercise: Go! Fight! Win!](@next)
